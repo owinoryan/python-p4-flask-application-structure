e9e2f12be0aded1da62a2318779a943cb26e5a41
ryanowino <owinoryan@gmail.com>
2024-10-01 16:15:50 +0300

    lab done

--- server/app.py
+++ server/app.py
@@ -3,3 +3,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route("/<username>")
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__=="__main__":
+    app.run(debug=True)
+    
